12a5ab8e19d02ed73376461ef1bbb34ac1db6c68
Nik Patil <nikpatildev@protonmail.com>
2021-12-11 11:16:31 +0530

    'container_added'

---
+++ src/containers/ProductComponet.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const productComponet = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default productComponet
---
+++ src/containers/ProductListing.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const productListing = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default productListing
