42858d60d7a5e329ab77a99a259e6b933eb0f1d2
Nik Patil <nikpatildev@protonmail.com>
2022-02-19 09:44:00 +0530

    .

--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { setProducts } from "../redux/actions/productActions";
 import ProductComponent from "./ProductComponent";
 
 const ProductListing = () => {
-  const products = useSelector((state) => state);
+  // const products = useSelector((state) => state);
   const dispatch = useDispatch();
 
   const fetchProducts = async () => {
@@ -20,9 +20,9 @@ const ProductListing = () => {
 
   useEffect(() => {
     fetchProducts();
-  }, []);
+  });
 
-  console.log("Products: ", products);
+  // console.log("Products: ", products);
   return (
     <div>
       <ProductComponent />
